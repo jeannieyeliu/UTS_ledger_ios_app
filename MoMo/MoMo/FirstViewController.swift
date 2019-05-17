3cc5c957d27b0c3a806019b1c9602ccb15bc833f
Ye.liu <ye.liu@digihealthinnovations.com>
2019-05-17 14:43:22 +1000

    init project of MoMo

---
+++ MoMo/MoMo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MoMo
+//
+//  Created by 刘烨 on 17/5/19.
+//  Copyright © 2019 Clima. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
