7c55ace69763bead43a66dce3b3309e70e6d634c
Ye.liu <ye.liu@digihealthinnovations.com>
2019-05-22 17:34:44 +1000

    demo for graph

--- MoMo/MoMo/Controller/StatisticViewController.swift
+++ MoMo/MoMo/Controller/StatisticViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
-
+import SwiftCharts
 class StatisticViewController: UIViewController {
 
+    @IBOutlet weak var chartContentView: UIView!
     @IBOutlet weak var tf_budget: UITextField!
+    var chartView: BarsChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let chartConfig = BarsChartConfig(valsAxisConfig:
+            ChartAxisConfig(from: 1, to: 7, by: 1))
+        
+        let frame = CGRect(x:0, y: 300, width: chartContentView.frame.width, height: chartContentView.frame.height)
+        
+        let chart = BarsChart(frame: frame,
+                              chartConfig: chartConfig, xTitle: "Week", yTitle: "Money Spent",
+                              bars: [("Mon", 125),
+                                     ("Tues", 250),
+                                     ("Wed", 25.8),
+                                     ("Thu", 215),
+                                     ("Wed", 23),
+                                     ("Sat", 300),
+                                     ("Sun", 278),
+                                    ],
+                              color: UIColor.red, barWidth: (15))
+        chartContentView.addSubview(chart.view)
+        self.chartView = chart
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
     /*
     // MARK: - Navigation
